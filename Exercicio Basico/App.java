0224662fd377f972594a35d2ea98703d45127171
Anna-Carla <annacarlateixeira16@gmail.com>
2023-08-29 10:51:31 -0300

    finalizado

--- Exercicio Basico/App.java
+++ Exercicio Basico/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Nomes das Equipes\nEquipe 1: ");
         String equipe1 = sc.nextLine();
 
@@ -14,18 +14,18 @@ public class App {
         Partida partida = new Partida(equipe1, equipe2);
 
         while (!partida.verificarEmpate()) {
-            for (int i = 1; i <= 4; i++) {
-                System.out.print("Pontos do Periodo\n");
-                System.out.print(equipe1+": ");
+            for (int i = 1; i < 4; i++) {
+                System.out.print("\nDigite a pontuação\n");
+                System.out.print(equipe1 + ": ");
                 int pontosEquipe1 = sc.nextInt();
-                System.out.print(equipe2+": ");
+                System.out.print(equipe2 + ": ");
                 int pontosEquipe2 = sc.nextInt();
 
                 partida.pontuacaoPeriodo(pontosEquipe1, pontosEquipe2);
             }
-
-            System.out.println(partida.placar());
+ 
         }
+        System.out.println(partida.placar());
 
         System.out.println("Equipe vencedora: " + partida.verificarVencedor());
 
--- Exercicio Basico/Partida.java
+++ Exercicio Basico/Partida.java
@@ -11,60 +11,28 @@ public class Partida {
     public Partida(String equipe1, String equipe2) {
         this.equipe1 = equipe1;
         this.equipe2 = equipe2;
+        this.periodo = 0;
     }
 
     public void pontuacaoPeriodo(int pontosEquipe1, int pontosEquipe2) {
-        if (!verificarEmpate()==true) {
+        if (!verificarEmpate()) {
             ptEquipe1.add(pontosEquipe1);
             ptEquipe2.add(pontosEquipe2);
             periodo++;
         }
     }
 
-    public String placar() {
-        String result = "\t\t\t1Q\t2Q\t3Q\t4Q\t";
-
-        for (int i = 1; i <= periodo - 4; i++) {
-            result += "PR" + i + "\t";
-        }
-
-        result += "FINAL\n" + equipe1 + tabEquipe(equipe1);
-        result += pontuacoesToString(ptEquipe1) + "\n";
-        result += equipe2 + tabEquipe(equipe2);
-        result += pontuacoesToString(ptEquipe2) + "\n";
-        return result;
-    }
-
-    private String tabEquipe(String equipe) {
-        return (equipe.length() / 8 > 0) ? "\t" : "\t\t";
-    }
-
-    private String pontuacoesToString(ArrayList<Integer> pontos) {
-        String result = "";
+        private int somarPontuacao(ArrayList<Integer> pontos) {
+        int somaPontos = 0;
         for (int i = 0; i < pontos.size(); i++) {
-            result += pontos.get(i) + "\t";
+            somaPontos += pontos.get(i);
         }
-        result += somarPontuacao(pontos) + "\n";
-        return result;
-    }
-
-    private int somarPontuacao(ArrayList<Integer> pontos) {
-        int soma = 0;
-        for (int i = 0; i < pontos.size(); i++) {
-            soma += pontos.get(i);
-        }
-        return soma;
+        return somaPontos;
     }
 
     public boolean verificarEmpate() {
         int empate = somarPontuacao(ptEquipe1) - somarPontuacao(ptEquipe2);
-        if(empate == 0 || periodo < 4) {
-            return false;    
-        } else {
-            verificarVencedor();
-            return true;
-        }
-        
+        return periodo >= 4 && empate != 0;
     }
 
     public String verificarVencedor() {
@@ -75,5 +43,31 @@ public class Partida {
         }
     }
 
+    public String placar() {
+        String result = "\t\t1Q\t2Q\t3Q\t4Q\t";
+
+        for (int i = 1; i <= periodo - 4; i++) {
+            result += "PR" + i + "\t";
+        }
+
+        result += "FINAL\n" + equipe1 + tabEquipe(equipe1);
+        result += pontuacaoString(ptEquipe1) + "\n";
+        result += equipe2 + tabEquipe(equipe2);
+        result += pontuacaoString(ptEquipe2) + "\n";
+        return result;
+    }
+
+    private String tabEquipe(String equipe) {
+        return (equipe.length() / 8 > 0) ? "\t" : "\t\t";
+    }
+
+    private String pontuacaoString(ArrayList<Integer> pontos) {
+        String result = "";
+        for (int i = 0; i < pontos.size(); i++) {
+            result += pontos.get(i) + "\t";
+        }
+        result += somarPontuacao(pontos) + "\n";
+        return result;
+    }
 
 }
