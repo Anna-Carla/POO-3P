8a9f1a6b5139c56fb172c3285e4525718d24e072
Anna-Carla <annacarlateixeira16@gmail.com>
2023-10-05 12:45:21 -0300

    tentando validar o convite

--- Exercicio 2/Convidado.java
+++ Exercicio 2/Convidado.java
@@ -4,7 +4,6 @@ public class Convidado extends Frequentador {
     private ArrayList<Convite> convites;
     protected boolean presenca = false;
 
-
     public Convidado(String nome) {
         super(nome);
         this.convites = new ArrayList<>();
@@ -12,17 +11,17 @@ public class Convidado extends Frequentador {
 
     @Override
     public void registrarVisita(Data data, Hora horaEntrada) {
-        if(pesquisarConvite(data)) {
-        Visita visita = new Visita(this, data, horaEntrada);
-        presenca = true;
-        visitas.add(visita);
-        System.out.println("O Convidado Entrou");
+        if (pesquisarConvite(data)) {
+            Visita visita = new Visita(this, data, horaEntrada);
+            presenca = true;
+            visitas.add(visita);
+            System.out.println("O Convidado Entrou");
         }
     }
 
     private boolean pesquisarConvite(Data data) {
         for (Convite convite : convites) {
-            if (convite.getData().equals(data) && convite.getSocio().isPresenca()==true) {
+            if (convite.getData().equals(data) && convite.getSocio().isPresenca()) {
                 System.out.println("Convite existente");
                 return true;
             }
